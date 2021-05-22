e0984e18ff57bacee06a7dc6111e7482cac7d273
z1103246 <Zhaosteven9@gmail.com>
2021-05-22 18:43:26 -0500

    Fixed circle bar animation and text

--- frontend/src/SkillPie.js
+++ frontend/src/SkillPie.js
@@ -15,24 +15,26 @@ class SkillBar extends React.Component {
         const cbar = (100 - percent) / 100 * c;
 
         this.cbarRef.current.animate(
-            [{ strokeDashoffset: 0 }, { strokeDashoffset: cbar }], { duration: 1000, easing: 'linear' }
+            [{ strokeDashoffset: c }, { strokeDashoffset: cbar }], { duration: 1000, easing: 'linear' }
         );
-
-        this.cbarRef.current.style.strokeDashoffset = c;
-        this.cbarRef.current.style.strokeDasharray = c;
 
         this.cbarRef.current.style.transitionDuration = '0.3s';
     }
 
     render() {
+        const r = 45;
+        const c = Math.PI * (r * 2);
+        const percent = this.props.percent;
+        const cbar = (100 - percent) / 100 * c;
+
         return (
             <li>
                 <svg viewBox="0 0 100 100">
-                    <circle cx="50" cy="50" r="45"></circle>
-                    <circle className="cbar" cx="50" cy="50" r="45" ref={this.cbarRef}></circle>
-                    <span>{this.props.name}</span>
-                    <small ref={this.smallRef}>{`${this.props.percent}%`}</small>
+                    <circle cx="50" cy="50" r={r}></circle>
+                    <circle className="cbar" cx="50" cy="50" r={r} strokeDashoffset={cbar} strokeDasharray={c} ref={this.cbarRef}></circle>
                 </svg>
+                <span>{this.props.name}</span>
+                <small ref={this.smallRef}>{`${this.props.percent}%`}</small>
             </li>
         );
     }
